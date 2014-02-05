a6c0759624095b68f63c94e1966b82eb3f7e5150
Michael Hernandez <michael.hernandez1988@gmail.com>
2014-02-05 23:53:05 +0000

    Quick change to one of the tests

--- test/random/d.js
+++ test/random/d.js
@@ -1,13 +1,20 @@
-db = require('./index')('data');
+db = require('../../index')('data');
 db.open('testbatch');
 
 batch = db.batch();
 
-batch.put('one', 'two')
+batch = batch.put('one', 'two')
 .put('two', 'two')
 .put('three', 'three')
 .delete('two','two')
-.write();
+
+console.log(
+    db.get('one')
+  , db.get('two')
+  , db.get('three')
+);
+
+batch.write();
 
 console.log(
     db.get('one')
